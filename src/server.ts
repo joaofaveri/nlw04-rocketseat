ae73e55f2aa1a6dd262300282b19ed54243bd714
joaofaveri <joao.faveri@gmail.com>
2022-07-04 14:57:02 -0300

    Route post

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello, World!'})
+})
+
+app.post('/', (request, response) =>{
+    return response.json({message: 'Dados salvos com sucesso!'})
+})
+
 app.listen(5000, () => console.log('Server is running'))
