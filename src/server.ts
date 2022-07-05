db4f50fb72fac10ad41c8942ae687321d729502a
joaofaveri <joao.faveri@gmail.com>
2022-07-05 14:42:00 -0300

    ESlint and Prettier config

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from 'express'
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello, World!'})
+  return response.json({ message: 'Hello, World!' })
 })
 
-app.post('/', (request, response) =>{
-    return response.json({message: 'Dados salvos com sucesso!'})
+app.post('/', (request, response) => {
+  return response.json({ message: 'Dados salvos com sucesso!' })
 })
 
 app.listen(5000, () => console.log('Server is running'))
