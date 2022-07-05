44560f04d17595b144c2f3ab81992e51dd1a9d5f
joaofaveri <joao.faveri@gmail.com>
2022-07-05 19:59:55 -0300

    CreateUser migration

--- src/database/app-data-source.ts
+++ src/database/app-data-source.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm'
 const npsDataSource = new DataSource({
   type: 'sqlite',
   database: './src/database/database.sqlite',
+  migrations: ['./src/database/migrations/*.ts'],
 })
 
 export default npsDataSource
