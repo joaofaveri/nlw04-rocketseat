0f75fe6e7fe24c0091f0453c74d2fc66892f01e4
joaofaveri <joao.faveri@gmail.com>
2022-07-06 15:45:43 -0300

    Function create in UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express'
+import { User } from '../models/User'
+import npsDataSource from '../database/app-data-source'
 
 class UserController {
   async create(request: Request, response: Response) {
-    const data = request.body
-    console.log(data)
-    return response.send()
+    const { name, email } = request.body
+
+    const userRepository = npsDataSource.getRepository(User)
+
+    const user = userRepository.create({
+      name,
+      email,
+    })
+
+    await userRepository.save(user)
+
+    return response.json(user)
   }
 }
 
--- src/database/app-data-source.ts
+++ src/database/app-data-source.ts
@@ -4,6 +4,7 @@ const npsDataSource = new DataSource({
   type: 'sqlite',
   database: './src/database/database.sqlite',
   migrations: ['./src/database/migrations/*.ts'],
+  entities: ['./src/models/**.ts'],
 })
 
 export default npsDataSource
