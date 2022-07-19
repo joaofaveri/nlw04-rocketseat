13a6bcd3affdcae0d6d872dd279e37893f75ab70
joaofaveri <joao.faveri@gmail.com>
2022-07-19 00:23:16 -0300

    Create route for sendmail

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express'
+import { SendMailController } from './controller/SendMailController'
 import { SurveyController } from './controller/SurveyController'
 import { UserController } from './controller/UserController'
 
 const router = Router()
 const userController = new UserController()
 const surveyController = new SurveyController()
+const sendMailController = new SendMailController()
 
 router.post('/users', userController.create)
 
 router.get('/surveys', surveyController.show)
 router.post('/surveys', surveyController.create)
+
+router.post('/sendmail', sendMailController.execute)
 
 export { router }
